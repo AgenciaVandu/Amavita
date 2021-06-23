12f57a51ae8d72bb9377451d4c8cae549699aace
alvarbu <74802594+alvarbu@users.noreply.github.com>
2021-06-22 21:16:01 -0500

    tenia un error en el css, pero ya esta resuelto

--- resources/views/layouts/amavita.blade.php
+++ resources/views/layouts/amavita.blade.php
@@ -12,7 +12,7 @@
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.6.0/dist/css/bootstrap.min.css" integrity="sha384-B0vP5xmATw1+K9KRQjQERJvTumQW0nPEzvF6L/Z6nronJ3oUOFUFpCjEUQouq2+l" crossorigin="anonymous">
 </head>
 
-<nav class="navbar navbar-expand-lg navbar-dark" style="background-color: #EE7768">
+<nav class="navbar navbar-expand-lg navbar-dark bg-nav">
     <div class="container">
         <a class="navbar-brand" href="#">
             <img src="{{ asset('img/amavita-logo.svg')}}" width="200" alt="logotipo Amavita Caucel">
