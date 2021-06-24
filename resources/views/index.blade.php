82bd32362ec8ff8ac1b5c9f266f6c17277525886
alvarbu <74802594+alvarbu@users.noreply.github.com>
2021-06-23 21:07:20 -0500

    amavita landing preliminar

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -7,5 +7,243 @@
     </div>
 </header>
 
+<section id="ama-vivir-en-caucel" class="mt-5">
+    <div class="ama-vivir">
+        <div class="container">
+            <div class="titular">
+                <img src="{{asset('img/ama-vivir.svg') }}" alt="Ama vivir en Caucel | Amtiavita " class="img-fluid" width="480">
+            </div>
+        </div>
+    </div>
+    <div class="hight-lights">
+        <div class="container">
+            <div class="row">
+                <div class="col-lg-4 col-md-6 col-sm-6">
+                    <div class="row">
+                        <div class="col-lg-6 col-6 col-sm-6">
+                            <div class="icon">
+                                <img src="{{asset('img/caracteristicas/caucel.svg') }}" class="img-fluid" width="110" alt="">
+                                <h5 class="hurmebold">Caucel</h5>
+                                
+                            </div>
+                        </div>
+                        <div class="col-lg-6 col-6 col-sm-6">
+                            <div class="icon">
+                                <img src="{{asset('img/caracteristicas/hectareas.svg') }}" class="img-fluid" width="110" alt="">
+                                <h5 class="hurmebold">6.8<br> <span class="hurmeregular">Perimetral</span></h5>
+                            
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <div class="col-lg-4 col-md-6 col-sm-6">
+                    <div class="row">
+                        <div class="col-lg-6 col-6 col-sm-6">
+                            <div class="icon">
+                                <img src="{{asset('img/caracteristicas/viviendas.svg') }}" class="img-fluid" width="110" alt="">
+                                <h5 class="hurmebold">441<br> <span class="hurmeregular">viviendas</span></h5>
+                            
+                            </div>
+                        </div>
+                        <div class="col-lg-6 col-6 col-sm-6">
+                            <div class="icon">
+                                <img src="{{asset('img/caracteristicas/caseta.svg') }}" class="img-fluid" width="110" alt="">
+                                <h5 class="hurmebold">Caseta<br> <span class="hurmeregular">de vigilancia</span></h5>
+                                 
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <div class="col-lg-4 col-md-6 col-sm-6">
+                    <div class="row">
+                        <div class="col-lg-6 col-6 col-sm-6">
+                            <div class="icon">
+                                <img src="{{asset('img/caracteristicas/barda.svg') }}" class="img-fluid" width="110" alt="">
+                                <h5 class="hurmebold">Barda <br> <span class="hurmeregular">Perimetral</span></h5>
+                                
+                            </div>
+                        </div>
+                        <div class="col-lg-6 col-6 col-sm-6">
+                            <div class="icon">
+                                <img src="{{asset('img/caracteristicas/servicios.svg') }}" class="img-fluid" width="110" alt="">
+                                <h5 class="hurmebold">Todos los servicios<br> <span class="hurmeregular">a tu alcance</span></h5>
+                               
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</section>
+
+<section id="modelos" class="mt-5">
+    <div class="container">
+        <h1 style="text-align: center" class="hurmebold">Nuestros Modelos</h1>
+       <div class="modelos">
+        <div class="row">
+            <div class="col-lg-4 col-md-4 col-sm-12">
+                <div class="modelo">
+                    <img src="{{asset('img/ALULA.png') }}" class="img-fluid" alt="">
+                </div>
+                <div class="especificaciones">
+                        <p  class="hurmeregular">Construcción: <span class="hurmebold">50.93 m2</span><br>
+                        <span  class="hurmeregular">Terreno desde: <span class="hurmebold">5 x 20 m</span></span></p>
+                        <p></p>
+                    <li class="hurmeregular">2 recámaras</li>
+                    <li class="hurmeregular">Baño completo</li>
+                    <li class="hurmeregular">Pasillo lateral</li>
+                    <li class="hurmeregular">Patio</li>
+                    </div>
+            </div>
+            <div class="col-lg-4 col-md-4 col-sm-12">
+                <div class="modelo">
+                    <img src="{{asset('img/BOREAL.png') }}" class="img-fluid" alt="">
+                </div>
+                <div class="especificaciones">
+                        <p  class="hurmeregular">Construcción: <span class="hurmebold">61.86 m2</span><br>
+                        <span  class="hurmeregular">Terreno desde: <span class="hurmebold">6 x 20 m</span></span></p>
+                        <p></p>
+                    <li class="hurmeregular">2 recámaras</li>
+                    <li class="hurmeregular">Baño completo</li>
+                    <li class="hurmeregular">Pasillo lateral</li>
+                    <li class="hurmeregular">Patio</li>
+                    </div>
+            </div>
+            <div class="col-lg-4 col-md-4 col-sm-12">
+                <div class="modelo">
+                    <img src="{{asset('img/CITALA.png') }}" class="img-fluid" alt="">
+                </div>
+                <div class="especificaciones">
+                        <p  class="hurmeregular">Construcción: <span class="hurmebold">69.86 m2</span><br>
+                        <span  class="hurmeregular">Terreno desde: <span class="hurmebold">5.5 x 20 m</span></span></p>
+                        <p></p>
+                    <li class="hurmeregular">2 recámaras</li>
+                    <li class="hurmeregular">1 y 1/2 Baños</li>
+                    <li class="hurmeregular">Amplio patio de: <span class="hurmebold"> 39m2</span></li>
+                    <li class="hurmeregular">Pasillo lateral</li>
+                    </div>
+            </div>
+        </div>
+       </div>
+    </div>
+
+</section>
+
+<section id="beneficios">
+    <div class="bg-beneficios">
+        <div class="container">
+            <div class="titular">
+                <h1 class="hurmebold">Beneficios</h1>
+            </div>
+        </div>
+    </div>
+    <div class="container">
+        <div class="contenido-beneficios">
+            <div class="row ">
+                <div class="col-lg-6 col-md-6 col-sm-6 sinpadding">
+                    <div class="row ">
+                        <!--Elementos-->
+                        <div class="col-lg-4 col-md-4 col-sm-4 sinpadding">
+                            <div class="beneficios-ico">
+                                <img src="{{asset('img/beneficios/1.svg') }}" alt="" class="img-fluid" width="100">
+                            </div>
+                        </div>
+                        <div class="col-lg-8 col-md-8 col-sm-8 sinpadding">
+                            <h4 class="beneficio hurme-semibold">Supermercados</h4>
+                        </div>
+                        <!--Elementos-->
+                        <div class="col-lg-4 col-md-4 col-sm-4 sinpadding">
+                            <div class="beneficios-ico">
+                                <img src="{{asset('img/beneficios/3.svg') }}" alt="" class="img-fluid" width="100">
+                            </div>
+                        </div>
+                        <div class="col-lg-8 col-md-8 col-sm-8 sinpadding">
+                            <h4 class="beneficio hurme-semibold ">Centros Comreciales</h4>
+                        </div>
+                        <!--Elementos-->
+                        <div class="col-lg-4 col-md-4 col-sm-4 sinpadding">
+                            <div class="beneficios-ico">
+                                <img src="{{asset('img/beneficios/4.svg') }}" alt="" class="img-fluid" width="100">
+                            </div>
+                        </div>
+                        <div class="col-lg-8 col-md-8 col-sm-8 sinpadding">
+                            <h4 class="beneficio hurme-semibold ">Escuelas</h4>
+                        </div>
+                        <!--Elementos-->
+                        <div class="col-lg-4 col-md-4 col-sm-4 sinpadding">
+                            <div class="beneficios-ico">
+                                <img src="{{asset('img/beneficios/6.svg') }}" alt="" class="img-fluid" width="100">
+                            </div>
+                        </div>
+                        <div class="col-lg-8 col-md-8 col-sm-8 sinpadding">
+                            <h4 class="beneficio hurme-semibold ">Entretenimiento</h4>
+                        </div>
+                    </div>
+                </div>
+                <div class="col-lg-6 col-md-6 col-sm-6 sinpadding">
+                    <div class="row">
+                        <!--Elementos-->
+                        <div class="col-lg-4 col-md-4 col-sm-4 sinpadding">
+                            <div class="beneficios-ico">
+                                <img src="{{asset('img/beneficios/2.svg') }}" alt="" class="img-fluid" width="100">
+                            </div>
+                        </div>
+                        <div class="col-lg-8 col-md-8 col-sm-8 sinpadding">
+                            <h4 class="beneficio hurme-semibold ">Bancos</h4>
+                        </div>
+                        <!--Elementos-->
+                        <div class="col-lg-4 col-md-4 col-sm-4 sinpadding">
+                            <div class="beneficios-ico">
+                                <img src="{{asset('img/beneficios/8.svg') }}" alt="" class="img-fluid" width="100">
+                            </div>
+                        </div>
+                        <div class="col-lg-8 col-md-8 col-sm-8  sinpadding">
+                            <h4 class="beneficio hurme-semibold">Gasolineras</h4>
+                        </div>
+                        <!--Elementos-->
+                        <div class="col-lg-4 col-md-4 col-sm-4 sinpadding">
+                            <div class="beneficios-ico">
+                                <img src="{{asset('img/beneficios/5.svg') }}" alt="" class="img-fluid" width="100">
+                            </div>
+                        </div>
+                        <div class="col-lg-8 col-md-8 col-sm-8  sinpadding">
+                            <h4 class="beneficio hurme-semibold">Hospitales y Farmacias</h4>
+                        </div>
+                        <!--Elementos-->
+                        <div class="col-lg-4 col-md-4 col-sm-4 sinpadding">
+                            <div class="beneficios-ico">
+                                <img src="{{asset('img/beneficios/7.svg') }}" alt="" class="img-fluid" width="100">
+                            </div>
+                        </div>
+                        <div class="col-lg-8 col-md-8 col-sm-8  sinpadding">
+                            <h4 class="beneficio hurme-semibold">Pago de Servicios</h4>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</section>
+
+<section id="contactanos" class="mt-5">
+    <h1 style="text-align: center" class="hurmebold">Contáctanos</h1>
+    <div class="bg-contacto">
+        <div class="container">
+            <div class="row">
+                <div class="col-lg-6 col-md-6 col-sm-12">
+                    <div class="mapa">
+                        <img src="{{asset('img/mapa.svg') }}" alt="">
+                    </div>
+                </div>
+                <div class="col-lg-6 col-md-6 col-sm-12">
+
+                </div>
+            </div>
+        </div>
+    </div>
+
+</section>
+
 @endsection
 
--- resources/views/layouts/amavita.blade.php
+++ resources/views/layouts/amavita.blade.php
@@ -5,38 +5,40 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>Amavita Caucel</title>
+    <meta name="author" content="https://agenciavandu.com" />
 
     <link href="{{asset('css/style.css') }}" rel="stylesheet">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.3/css/all.min.css">
 
     <!--Bootstrap 4.6-->
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.6.0/dist/css/bootstrap.min.css" integrity="sha384-B0vP5xmATw1+K9KRQjQERJvTumQW0nPEzvF6L/Z6nronJ3oUOFUFpCjEUQouq2+l" crossorigin="anonymous">
 </head>
 
-<nav class="navbar navbar-expand-lg navbar-dark bg-nav fixed-top">
-    <div class="container">
-        <a class="navbar-brand" href="#">
-            <img src="{{ asset('img/amavita-logo.svg')}}" width="200" alt="logotipo Amavita Caucel">
-          </a>
-        <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
-          <span class="navbar-toggler-icon"></span>
-        </button>
-        <div class="collapse navbar-collapse" id="navbarNav">
-          <ul class="navbar-nav ml-auto">
-            <li class="nav-item active">
-              <a class="nav-link" href="#">Home <span class="sr-only">(current)</span></a>
-            </li>
-            <li class="nav-item">
-              <a class="nav-link" href="#">Features</a>
-            </li>
-            <li class="nav-item">
-              <a class="nav-link" href="#">Pricing</a>
-            </li>
-            <li class="nav-item">
-              <a class="nav-link disabled" href="#" tabindex="-1" aria-disabled="true">Disabled</a>
-            </li>
-          </ul>
-        </div>
+<nav class="navbar navbar-expand-lg navbar-dark bg-nav fixed-top patron">
+  <div class="container">
+    <a class="navbar-brand" href="#">
+        <img src="{{ asset('img/amavita-logo.svg')}}" width="200" alt="logotipo Amavita Caucel">
+      </a>
+    <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+      <span class="navbar-toggler-icon"></span>
+    </button>
+    <div class="collapse navbar-collapse" id="navbarNav">
+      <ul class="navbar-nav ml-auto">
+        <li class="nav-item active hurmeregular">
+          <a class="nav-link" href="#ama-vivir-en-caucel">Desarrollos <span class="sr-only">(current)</span></a>
+        </li>
+        <li class="nav-item active hurmeregular">
+          <a class="nav-link" href="#modelos">Modelos</a>
+        </li>
+        <li class="nav-item active hurmeregular">
+          <a class="nav-link" href="#beneficios">Beneficios</a>
+        </li>
+        <li class="nav-item active hurmeregular">
+          <a class="nav-link" href="#contactanos">Contacto</a>
+        </li>
+      </ul>
     </div>
+</div>
   </nav>
 
 
@@ -46,6 +48,34 @@
 <body>
 
     @yield('content')
+
+    <footer>
+      <div class="bg-footer">
+        <div class="container contenido-footer">
+          <div class="row align-items-start justify-content-center">
+            <div class="col-lg-6 col-md-6 col-sm-12">
+              <p class="footer-txt">
+                <span style="color: #fff"><b>Visita nuestro punto de venta</b></span> <br>
+                <span style="color: #fff">Plaza 55: C 59A Local 11 Cd. Caucel</span> <br>
+                <span style="color: #fff"><b>Informes: </b>Tel. (999) 944 9952 punto de venta</span> <br>
+                <span style="color: #fff">
+                  <b>
+                  www.grupocasitas.com.mx
+                </b></span> <br>
+                <span style="color: #fff"><i class="fab fa-facebook-square" style="margin-right: 5px"></i><i class="fab fa-instagram-square" style="margin-right: 5px"></i>GrupoCasitas</span>
+              </p>
+            </div>
+            <div class="col-lg-6 col-md-6 col-sm-12 pt-4">
+              
+                <div class="casitas">
+                  <img src="{{ asset('img/casitas.svg')}}" class="img-fluid" alt="Grupo Casitas" width="300">
+                </div>
+              
+            </div>
+          </div>
+        </div>
+      </div>
+    </footer>
 
 
     <script src="https://code.jquery.com/jquery-3.5.1.slim.min.js" integrity="sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj" crossorigin="anonymous"></script>
